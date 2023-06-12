355f2f415112f90d07178341c1c63bf0d457771c
iDkGK <42490029+iDkGK@users.noreply.github.com>
2023-06-12 17:06:18 +0800

    Add functions of getting ip and ipv6 address

--- interface/interface.py
+++ interface/interface.py
@@ -67,8 +67,62 @@ def get_iface_mac_address(iface_name: str) -> str:
                 iface_name,
                 tabulate.tabulate(
                     iface_mac_mappings.items(),
-                    headers=["Interface Name", "Mac Address"],
+                    headers=["Interface Name", "MAC Address"],
                 ),
             )
         )
     return iface_mac_address
+
+
+def get_iface_ip_address(iface_name: str) -> str:
+    """
+    Get mac address of specific interface
+
+    Args:
+    - ``iface_name``: interface name
+
+    Returns:
+    - Mac address of ``iface_name``
+
+    Raises:
+    - ValueError
+    """
+    iface_ip_address = iface_ip_mappings.get(iface_name)
+    if iface_ip_address is None:
+        raise ValueError(
+            "error getting interface {}. Available interfaces are listed below:\n{}".format(
+                iface_name,
+                tabulate.tabulate(
+                    iface_ip_mappings.items(),
+                    headers=["Interface Name", "IP Address"],
+                ),
+            )
+        )
+    return iface_ip_address
+
+
+def get_iface_ipv6_address(iface_name: str) -> str:
+    """
+    Get mac address of specific interface
+
+    Args:
+    - ``iface_name``: interface name
+
+    Returns:
+    - Mac address of ``iface_name``
+
+    Raises:
+    - ValueError
+    """
+    iface_ipv6_address = iface_ip_mappings.get(iface_name)
+    if iface_ipv6_address is None:
+        raise ValueError(
+            "error getting interface {}. Available interfaces are listed below:\n{}".format(
+                iface_name,
+                tabulate.tabulate(
+                    iface_ip_mappings.items(),
+                    headers=["Interface Name", "IPv6 Address"],
+                ),
+            )
+        )
+    return iface_ipv6_address
